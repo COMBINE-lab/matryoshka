0cd7b99da4fe583d74a5fca66a263a72d89ae382
Rob Patro <rob.patro@gmail.com>
2015-12-18 22:39:15 -0500

    fix compilation on OSX

--- src/MatryoshkaUtil.cpp
+++ src/MatryoshkaUtil.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
 	Authors: Darya Filippova, Geet Duggal, Rob Patro
 	dfilippo | geet | robp @cs.cmu.edu
 */
@@ -29,7 +29,7 @@ string matrix_format_error = "Invalid matrix format. Expected format: \n<chromoI
 MatrixProperties parseGZipMatrix(string path) {
 	MatrixProperties prop;
 
-    prop.matrix = make_shared<SparseMatrix>();
+    prop.matrix = std::make_shared<SparseMatrix>();
 
 	ifstream file(path, ios_base::in | ios_base::binary);
     assert(file.good());
@@ -47,8 +47,8 @@ MatrixProperties parseGZipMatrix(string path) {
     	vector<string> parts;
         boost::trim(line);
 		boost::split(parts, line, boost::is_any_of("\t"));
-        
-        if (firstLine) { 
+
+        if (firstLine) {
             if (parts.size() - 3 < 100) {
                 cerr << "[INFO] Matrix is smaller than the recommended minimum size of 101." << endl;
                 // exit(1);
@@ -73,7 +73,7 @@ MatrixProperties parseGZipMatrix(string path) {
             cerr << prop.chrom << " at resolution " << prop.resolution << "bp" << endl;
             firstLine = false;
         }
-        
+
         for (size_t j : boost::irange(3 + i, parts.size())) {
             double e  = stod(parts[j]);
             if (e > 0.0) {
@@ -81,7 +81,7 @@ MatrixProperties parseGZipMatrix(string path) {
                 prop.matrix->push_back(i, row, e);
                 tot += e;
                 nedge++;
-            }   
+            }
         }
 
     	++i;
@@ -99,9 +99,9 @@ void MatrixProperties::computemuBI() {
     int n = matrix->size1();
     BI = std::vector<double>(matrix->size1()+1);
 
-    double interval = 3; 
+    double interval = 3;
     double steps = 12;
-    double b = 0, a1, a2; 
+    double b = 0, a1, a2;
     muBI = 0;
     for (int i=0; i<n; i++) {
         b = 0;
@@ -110,22 +110,22 @@ void MatrixProperties::computemuBI() {
                 a1 = 0;
                 a2 = 0;
                 if (i-q >= 0) {
-                    if (i-p >= 0) 
+                    if (i-p >= 0)
                         a1 = M(i-q, i-p);
                     if (i+p < n)
-                        a1 -= M(i-q, i+p); 
+                        a1 -= M(i-q, i+p);
                 }
                 if (i+q < n) {
                     if (i-p >= 0)
                         a2 = M(i+q, i-p);
                     if (i+p < n)
-                        a2 -= M(i+q, i+p); 
+                        a2 -= M(i+q, i+p);
                 }
                 b += abs(a1) + abs(a2);
             }
         }
         BI[i] = b;
-        muBI += b; 
+        muBI += b;
     }
     muBI /= n;
     cerr << "Mean Boundary Index is " << muBI << endl;
@@ -153,7 +153,7 @@ DomainSet consensusDomains(WeightedDomainEnsemble& dEnsemble) {
     for (auto domainPersistence : pmap) {
         auto domain = domainPersistence.first;
         auto persistence = domainPersistence.second;
-        ivals.push_back(WeightedInterval(domain.start, domain.end, persistence));      
+        ivals.push_back(WeightedInterval(domain.start, domain.end, persistence));
     }
 
     IntervalScheduler scheduler(ivals);
@@ -170,7 +170,7 @@ DomainSet consensusDomains(WeightedDomainEnsemble& dEnsemble) {
 }
 
 //used for calculating domains across gammas in whole matrix
-WeightedDomainEnsemble multiscaleDomains(MatrixProperties matProp, 
+WeightedDomainEnsemble multiscaleDomains(MatrixProperties matProp,
     float gammaMax, float gammaMin, double stepSize, int k, int minMeanSamples) {
 
     auto A = matProp.matrix;
@@ -183,7 +183,7 @@ WeightedDomainEnsemble multiscaleDomains(MatrixProperties matProp,
     for (double gamma=gammaMin; gamma <= gammaMax+eps; gamma+=stepSize) {
 
         cerr << "gamma=" << gamma << endl;
- 
+
         MatryoshkaParams params(A, gamma, k, minMeanSamples); // k parameter is not used for anything in Params
         MatryoshkaDAG G(params, BI, muBI); // but is used in the DAG
         G.build();
@@ -201,13 +201,13 @@ WeightedDomainEnsemble multiscaleDomains(MatrixProperties matProp,
 
 //used for calculating domains at hierarchical level after gamma clustering
 WeightedDomainEnsemble multiscaleDomains(MatrixProperties matProp,
-    float gammaMax, float gammaMin, double stepSize, int k, int minMeanSamples, 
+    float gammaMax, float gammaMin, double stepSize, int k, int minMeanSamples,
     int areaCovered, std::vector<std::vector<double>> allMu, int index) {
 
     auto A = matProp.matrix;
     double muBI = matProp.muBI;
     std::vector<double> BI = matProp.BI;
-    
+
     WeightedDomainEnsemble dEnsemble;
     double eps = 1e-5;
     int ind = index;
@@ -248,11 +248,11 @@ int outputDomains(DomainSet dSet, string fname, MatrixProperties matProp, int hi
     file.open(fname, ios::app);
     int res = matProp.resolution;
     for (auto d : dSet) {
-        //file << myIndex << "\t" << matProp.chrom << "\t" << (d.start)*res << "\t" << 
+        //file << myIndex << "\t" << matProp.chrom << "\t" << (d.start)*res << "\t" <<
         //    (d.end)*res << "\t" << pIndex << "\t" << hier << endl;
         //file << chromosome << tool << feature << start << end << score << strand << frame << hier << endl;
         file << matProp.chrom << "\t" << "Matryoshka1.0" << "\t" << "Domain" << "\t" << (d.start)*res << "\t" <<
-            (d.end)*res << "\t" << "." << "\t" << "." << "\t" << "." << "\t" << 
+            (d.end)*res << "\t" << "." << "\t" << "." << "\t" << "." << "\t" <<
             "Level=" << hier << ";ID=" << myIndex;
         if(pIndex != -1)
             file << ";Parent=" << pIndex << endl;
@@ -268,7 +268,7 @@ int outputDomains(DomainSet dSet, string fname, MatrixProperties matProp, int hi
 int calCoverage(WeightedDomainEnsemble& dEnsemble, MatrixProperties matProp) {
     DomainSet allDomainSet; //across all gamma values
     int res = matProp.resolution;
-    
+
     for (auto dSetIdx : boost::irange(size_t{0}, dEnsemble.domainSets.size())) {
         auto& dSet = dEnsemble.domainSets[dSetIdx];
         for (auto d : dSet)
@@ -276,7 +276,7 @@ int calCoverage(WeightedDomainEnsemble& dEnsemble, MatrixProperties matProp) {
     }
 
     sort(allDomainSet.begin(), allDomainSet.end());
-    
+
     //calculate coverage
     size_t curStart = allDomainSet[0].start;
     size_t curEnd = allDomainSet[0].end;
@@ -302,7 +302,7 @@ int calCoverage(WeightedDomainEnsemble& dEnsemble, MatrixProperties matProp) {
 }
 
 std::vector<double> getMu(std::shared_ptr<SparseMatrix> A, float gamma, int minMeanSamples){
-    MatryoshkaParams params(A, gamma, 0, minMeanSamples); 
+    MatryoshkaParams params(A, gamma, 0, minMeanSamples);
     return params.mu;
 }
 
@@ -337,7 +337,7 @@ double getVI(DomainSet dSet1, DomainSet dSet2, size_t N){
     double MI = 0, VI = 0;
     size_t minD = dSet1[0].start;
     size_t maxD = dSet1[0].end;
-    
+
     for (auto d : dSet1) {
         if (minD > d.start) { minD = d.start; }
         if (maxD < d.end) { maxD = d.end; }
@@ -349,7 +349,7 @@ double getVI(DomainSet dSet1, DomainSet dSet2, size_t N){
     }
 
     size_t L = maxD - minD + 1, start = minD, end = 0;
-    
+
     DomainSet dSet1Plus;
     for (auto d1 : dSet1) {
         if (start < d1.start)
@@ -360,7 +360,7 @@ double getVI(DomainSet dSet1, DomainSet dSet2, size_t N){
     }
     if (end < maxD)
         dSet1Plus.push_back(Domain(end+1, maxD));
-   
+
     start = minD;
     DomainSet dSet2Plus;
     for (auto d2 : dSet2) {
@@ -372,7 +372,7 @@ double getVI(DomainSet dSet1, DomainSet dSet2, size_t N){
     }
     if (end < maxD)
         dSet2Plus.push_back(Domain(end+1, maxD));
-    
+
     std::vector<double> p_xs, p_ys;
     for (auto d1 : dSet1Plus)
         p_xs.push_back((d1.end + 1 - d1.start) * 1.0 / L);
@@ -388,13 +388,13 @@ double getVI(DomainSet dSet1, DomainSet dSet2, size_t N){
             auto d2 = dSet2Plus[j];
             p_y = p_ys[j];
             overlap = min(d2.end, d1.end) + 1 - max(d1.start, d2.start);
-            if (overlap <= 0) 
+            if (overlap <= 0)
                 continue;
             p_xy = (overlap * 1.0) / L;
             MI += (p_xy * log(p_xy / (p_x * p_y)));
         }
     }
-    
+
     double H_1 = 0, H_2 = 0;
     for (double p : p_xs)
         H_1 += p * log(p);
@@ -415,7 +415,7 @@ std::vector<int> getCluster(double **VI_S, int K){
     //to sort the vector, maintaining indices
     std::vector<int> indices;
     std::priority_queue<std::pair<double, int>> q_temp;
-    
+
     q_temp.push(std::pair<double, int>(0, 0));
     for (int i=0; i<K-1; i++)
         q_temp.push(std::pair<double, int>(VI_S[i][i+1], i+1));
@@ -446,7 +446,7 @@ std::vector<int> getCluster(double **VI_S, int K){
 
             avgWidth = calAvgWidth(VI_S, clusterid, K);
             if (avgWidth > tempWidth){
-                tempWidth = avgWidth; 
+                tempWidth = avgWidth;
                 std::copy(clusterid, clusterid + K, PAMresult);
             }
     }
@@ -462,7 +462,7 @@ void optK(SymmetricMatrix sums, int k, int clusterid[]) {
     for (size_t i : boost::irange(size_t{3},size_t(k+1))) {
         for (size_t j : boost::irange(size_t{0}, tempK))
             solDict[i][j] = 1000000;
-    }  
+    }
 
     std::map<std::pair<int, int>, int> opt;
     opt[std::make_pair(0, 0)] = 0;
@@ -484,7 +484,7 @@ void optK(SymmetricMatrix sums, int k, int clusterid[]) {
             opt[std::make_pair(l,i)] = x;
         }
     }
-    
+
     int lvl = k;
     int right = tempK-1;
     int left = opt[std::make_pair(lvl, right)] + 1;
@@ -529,7 +529,7 @@ double calAvgWidth(double **VI_S, int clusterid[], int K){
 
     for (int i=0; i<K; i++){
         memset(cDist, 0, K*sizeof(double));
-        
+
         for (int j=0; j<K; j++){
             curCluster = clusterid[j];
             cDist[curCluster] += abs(VI_S[i][j]);
@@ -538,7 +538,7 @@ double calAvgWidth(double **VI_S, int clusterid[], int K){
         myCluster = clusterid[i];
         if (cCount[myCluster] > 1)
             a = cDist[myCluster]/(cCount[myCluster]-1); //not counting self
-        else 
+        else
             a = 0;
         cDist[myCluster] = (std::numeric_limits<double>::max)();
         b = (std::numeric_limits<double>::max)();
@@ -554,7 +554,7 @@ double calAvgWidth(double **VI_S, int clusterid[], int K){
         else
             sil[i] = (b-a)/max(a,b);
     }
-    
+
     for (int i=0; i<K; i++)
         width += sil[i];
 
